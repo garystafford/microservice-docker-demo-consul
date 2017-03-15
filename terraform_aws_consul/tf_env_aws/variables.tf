b5a84aa741bd8d14e6b0380e4efd26aada279e66
Gary Stafford <garystafford@rochester.rr.com>
2017-03-15 01:06:34 -0400

    Refactoring tf scripts

--- terraform_aws_consul/tf_env_aws/variables.tf
+++ terraform_aws_consul/tf_env_aws/variables.tf
@@ -20,23 +20,18 @@ variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
 
-/*variable "consul_subnet_public_cidr" {
-  description = "CIDR for the Consul Subnet 1"
-  default = "10.0.0.0/24"
-}*/
-
 variable "consul_subnet_cidr1" {
   description = "CIDR for the Consul Subnet 1"
   default = "10.0.1.0/24"
 }
 
 variable "consul_subnet_cidr2" {
-  description = "CIDR for the Consul Subnet 1"
+  description = "CIDR for the Consul Subnet 2"
   default = "10.0.2.0/24"
 }
 
 variable "consul_subnet_cidr3" {
-  description = "CIDR for the Consul Subnet 1"
+  description = "CIDR for the Consul Subnet 3"
   default = "10.0.3.0/24"
 }
 
