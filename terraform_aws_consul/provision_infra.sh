43cf3ad24de86500304f09f5b90bd14b92455138
Gary Stafford <garystafford@rochester.rr.com>
2017-03-16 21:19:19 -0400

    Adding provision/destroy scripts

---
+++ terraform_aws_consul/provision_infra.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Provision infrastructure
+
+set -e
+
+echo "Building AWS infrastructure..."
+
+cd terraform_aws_consul/tf_env_aws/
+
+terraform remote config \
+  -backend=s3 \
+  -backend-config="bucket=tf-remote-state-gstafford" \
+  -backend-config="key=terraform_consul.tfstate" \
+  -backend-config="region=us-east-1"
+
+terraform plan
+terraform apply
+
+echo "Building AWS infrastructure complete..."
