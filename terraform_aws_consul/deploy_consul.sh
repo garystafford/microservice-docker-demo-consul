3446634cc12b6130739dc767efcaaa8c020ae5f5
Gary Stafford <garystafford@rochester.rr.com>
2017-03-17 01:13:10 -0400

    Fixing deploy script

--- terraform_aws_consul/deploy_consul.sh
+++ terraform_aws_consul/deploy_consul.sh
@@ -24,11 +24,9 @@ echo "consul-server-1 public ip: ${ec2_public_ip}"
 #   'echo export ec2_server1_private_ip="${ec2_server1_private_ip}" >> ~/.bashrc'
 
 ssh -oStrictHostKeyChecking=no -T -i ~/.ssh/consul_aws_rsa ubuntu@${ec2_public_ip} << EOSSH
-  export ec2_server1_private_ip="${ec2_server1_private_ip}"
-  export consul_server=consul-server-1
-  env
-
-  docker run -d \
+  ec2_server1_private_ip="${ec2_server1_private_ip}" \
+  && consul_server=consul-server-1 \
+  && docker run -d \
     --net=host \
     --hostname "${consul_server}" \
     --name "${consul_server}" \
